15212974959f9d3d5ebb22429f98556e70bc339c
ciaguy04 <jamie.andry@gmail.com>
2017-07-03 14:56:09 -0500

    new URL + view for adding to existing lists. FT passes :-)

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -96,7 +96,5 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-        self.fail('Finish the test!')
-
 
 
