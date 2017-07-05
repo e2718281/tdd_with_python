951b597398ebaf1b1976c7366684152481016698
ciaguy04 <jamie.andry@gmail.com>
2017-07-05 12:27:20 -0500

    adjust local and dev server configuration

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../database/db.sqlite3'),
     }
 }
 
