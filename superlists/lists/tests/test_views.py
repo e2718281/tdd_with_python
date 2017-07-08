2d10b47aae720aad6a37be8d01e3a48da89785ce
ciaguy04 <jamie.andry@gmail.com>
2017-07-08 14:40:14 -0500

    add jquery, qunit tests, list.js with keypress listeners

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.utils.html import escape
 from ..models import Item, List
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
+from ..forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
+from unittest import skip
 #from superlists.lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -81,7 +82,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/{}/'.format(list_.id))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def post_invalid_input(self):
@@ -102,11 +103,23 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/{}/'.format(list1.id),
+            data={'text': 'textey'}
+        )
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(),1)
 
 class NewListTest(TestCase):
 
